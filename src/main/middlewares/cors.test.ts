325ef5aa802821c6257537148980fdc8fa9e6772
Lucas Emanoel <lucws2002@gmail.com>
2023-10-16 18:26:06 -0400

    feat: ensure requests return json as default response in content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,12 +3,12 @@ import request from "supertest";
 
 describe('CORS Middleware', () => {
     test('should enable CORS', async () => {
-        app.post('/test_body_parser', (req, res) => {
+        app.post('/test_cors', (req, res) => {
             res.send()
         })
 
         await request(app)
-            .get('/test_body_parser')
+            .get('/test_cors')
             .expect('access-control-allow-origin', '*')
             .expect('access-control-allow-methods', '*')
             .expect('access-control-allow-headers', '*')
