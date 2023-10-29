0e4f3c5e55b2fa31fb7bbc881303df36ae1dc338
Lucas Emanoel <lucws2002@gmail.com>
2023-10-29 01:52:20 -0400

    style: comment

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,8 @@
 import {MongoHelper} from "../infra/db/mongodb/helpers/mongo-helper";
 import env from './config/env'
 
+// Entrypoint
+
 MongoHelper.connect(env.mongoUrl)
     .then(async () => {
         const app = (await import('./config/app')).default
